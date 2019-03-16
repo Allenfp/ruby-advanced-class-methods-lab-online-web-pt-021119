faed08aec01dd5c0765bd6819130795ff51d06a5
Allen F Partlow <allenfp@gmail.com>
2019-03-16 21:29:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,7 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    data = file_name.split(" - ")
+    data = filename.split(" - ")
     song_name = data[1].split(".").first
 
     song = Song.create_by_name(song_name)
