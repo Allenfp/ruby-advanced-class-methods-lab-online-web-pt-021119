dba890736abea86401c84d9a31d5b75475addbe3
Allen F Partlow <allenfp@gmail.com>
2019-03-16 21:22:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -44,7 +44,8 @@ class Song
   end
 
   def self.new_from_filename(filename)
-    filename.split(" - ")
+    artist = filename.split(" - ")[0]
+    song_name = filename.split(" - ")[1].chomp('.mp3')
   end
 
 
